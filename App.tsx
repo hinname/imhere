7f965639a67b2d86445a55b3ca29998850c87c63
hinname <herivelton.pro@gmail.com>
2024-04-13 19:00:34 -0300

    training stylesheet

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sabado, 13 de Abril de 2024</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24,
   },
+
+  eventName: {
+    color: '#fdfcfe',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  }
 });
