64b9f3a1d1e4c3e8844ed046848ae7a554213cff
hinname <herivelton.pro@gmail.com>
2024-04-13 21:38:39 -0300

    folder structure basic with Home screen

--- App.tsx
+++ App.tsx
@@ -1,32 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/screens/Home/Index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sabado, 13 de Abril de 2024</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24,
-  },
-
-  eventName: {
-    color: '#fdfcfe',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 48,
-  },
-
-  eventDate: {
-    color: '#6b6b6b',
-    fontSize: 16,
-  }
-});
